805e716dd6725415f69009430ce7cf78fed35a41
Jorge1 <agullojorge@gmail.com>
2024-03-25 20:07:53 +0100

    Added route params.

--- app/src/main/java/com/softyorch/tictactoecompose/ui/core/Routes.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/ui/core/Routes.kt
@@ -1,6 +1,10 @@
 package com.softyorch.tictactoecompose.ui.core
 
 sealed class Routes(val route: String) {
-    object Home: Routes("home")
-    object Game: Routes("game")
+    object Home : Routes("home")
+    object Game : Routes("game/{gameId}/{userId}/{owner}") {
+        fun createRoute(gameId: String, userId: String, owner: Boolean): String {
+            return "game/$gameId/$userId/$owner"
+        }
+    }
 }
