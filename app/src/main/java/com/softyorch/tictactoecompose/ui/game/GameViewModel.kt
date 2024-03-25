26f9b3cefa4709a4dcac4f5b094f05a49e2ae0fb
Jorge1 <agullojorge@gmail.com>
2024-03-25 20:42:05 +0100

    Created join game functions

--- app/src/main/java/com/softyorch/tictactoecompose/ui/game/GameViewModel.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/ui/game/GameViewModel.kt
@@ -1,10 +1,39 @@
 package com.softyorch.tictactoecompose.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.tictactoecompose.data.network.FirebaseService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GameViewModel @Inject constructor(): ViewModel() {
+class GameViewModel @Inject constructor(
+    private val firebaseService: FirebaseService
+) : ViewModel() {
 
+
+    lateinit var userId: String
+
+    fun joinToGame(gameId: String, userId: String, owner: Boolean) {
+        this.userId = userId
+        if (owner) {
+            joinToGameLikeOwner(gameId)
+        } else {
+            joinToGameLikeGuest(gameId)
+        }
+    }
+
+    private fun joinToGameLikeOwner(gameId: String) {
+        viewModelScope.launch {
+            firebaseService.joinToGame(gameId).collect {
+                Log.i("BeerChatLog", it.toString())
+            }
+        }
+    }
+
+    private fun joinToGameLikeGuest(gameId: String) {
+        TODO("Not yet implemented")
+    }
 }
