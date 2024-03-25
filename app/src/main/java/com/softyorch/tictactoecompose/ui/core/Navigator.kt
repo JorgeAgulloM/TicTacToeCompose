f4c1378589e91cffbc5858dae2535f8743ba525c
Jorge1 <agullojorge@gmail.com>
2024-03-25 20:09:57 +0100

    Implements data navigation

--- app/src/main/java/com/softyorch/tictactoecompose/ui/core/Navigator.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/ui/core/Navigator.kt
@@ -2,8 +2,10 @@ package com.softyorch.tictactoecompose.ui.core
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.softyorch.tictactoecompose.ui.core.Routes.Game
 import com.softyorch.tictactoecompose.ui.core.Routes.Home
 import com.softyorch.tictactoecompose.ui.game.GameScreen
@@ -14,10 +16,23 @@ fun ContentWrapper(navController: NavHostController) {
 
     NavHost(navController, startDestination = Home.route) {
         composable(Home.route) {
-            HomeScreen { navController.navigate(Game.route) }
+            HomeScreen { gameId, userId, owner ->
+                navController.navigate(Game.createRoute(gameId, userId, owner))
+            }
         }
-        composable(Game.route) {
-            GameScreen { navController.navigate(Home.route)}
+        composable(
+            Game.route,
+            arguments = listOf(
+                navArgument("gameId") { type = NavType.StringType },
+                navArgument("userId") { type = NavType.StringType },
+                navArgument("owner") { type = NavType.BoolType },
+            )
+        ) {
+            GameScreen(
+                gameId = it.arguments?.getString("gameId").orEmpty(),
+                userId = it.arguments?.getString("userId").orEmpty(),
+                owner = it.arguments?.getBoolean("owner") ?: false
+            ) { navController.navigate(Home.route) }
         }
     }
 }
