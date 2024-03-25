93f0824ceb9ed09bcee66633b99a600704551241
Jorge1 <agullojorge@gmail.com>
2024-03-25 08:30:53 +0100

    Injected firebase service

--- app/src/main/java/com/softyorch/tictactoecompose/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/ui/home/HomeViewModel.kt
@@ -1,11 +1,12 @@
 package com.softyorch.tictactoecompose.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.softyorch.tictactoecompose.data.network.FirebaseService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(private val firebaseService: FirebaseService): ViewModel() {
     fun onCreateGame() {
 
     }
