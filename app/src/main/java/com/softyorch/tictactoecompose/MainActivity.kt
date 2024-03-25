ba74c674fc1499cb38240ccd619c79633ca7c363
Jorge1 <agullojorge@gmail.com>
2024-03-25 07:10:05 +0100

    Renamed

--- app/src/main/java/com/softyorch/tictactoecompose/MainActivity.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.softyorch.tictactoecompose.ui.home.HomeScreen
 import com.softyorch.tictactoecompose.ui.theme.TicTacToeComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    HomeScreen()
                 }
             }
         }
