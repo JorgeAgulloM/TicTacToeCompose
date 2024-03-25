603b5dbcee2f725d88674d9e510e911a5c0c547e
Jorge1 <agullojorge@gmail.com>
2024-03-25 20:41:43 +0100

    Created join game firebase function

--- app/src/main/java/com/softyorch/tictactoecompose/data/network/FirebaseService.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/data/network/FirebaseService.kt
@@ -1,7 +1,12 @@
 package com.softyorch.tictactoecompose.data.network
 
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.snapshots
 import com.softyorch.tictactoecompose.data.network.model.GameData
+import com.softyorch.tictactoecompose.data.network.model.GameData.Companion.toModel
+import com.softyorch.tictactoecompose.ui.model.GameModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class FirebaseService @Inject constructor(private val reference: DatabaseReference) {
@@ -17,4 +22,11 @@ class FirebaseService @Inject constructor(private val reference: DatabaseReferen
         gameRef.setValue(newGame)
         return newGame.gameId.orEmpty()
     }
+
+    fun joinToGame(gameId: String): Flow<GameModel?> =
+        reference.database.reference.child("$PATH/$gameId")
+            .snapshots.map { dataSnapshot ->
+                dataSnapshot.getValue(GameData::class.java)?.toModel()
+            }
+
 }
