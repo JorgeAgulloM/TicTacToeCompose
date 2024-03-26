fd42fa9825ca5dc50803d48778716b6b7ded0c2f
Jorge1 <agullojorge@gmail.com>
2024-03-26 19:43:38 +0100

    Implemented game logic

--- app/src/main/java/com/softyorch/tictactoecompose/ui/game/GameViewModel.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/ui/game/GameViewModel.kt
@@ -19,9 +19,12 @@ class GameViewModel @Inject constructor(
     private val firebaseService: FirebaseService
 ) : ViewModel() {
 
-    lateinit var userId: String
+    private lateinit var userId: String
     private var _game = MutableStateFlow<GameModel?>(null)
     val game: StateFlow<GameModel?> = _game
+
+    private var _winner = MutableStateFlow<PlayerType?>(null)
+    val winner: StateFlow<PlayerType?> = _winner
 
     fun joinToGame(gameId: String, userId: String, owner: Boolean) {
         this.userId = userId
@@ -29,6 +32,24 @@ class GameViewModel @Inject constructor(
             joinToGameLikeOwner(gameId)
         } else {
             joinToGameLikeGuest(gameId)
+        }
+    }
+
+    fun onItemSelected(position: Int) {
+        val currentGame = _game.value ?: return
+
+        if (currentGame.isGameReady &&
+            currentGame.board[position] == PlayerType.Empty &&
+            isMyTurn(currentGame.playerTurn)
+        ) {
+            viewModelScope.launch {
+                val updateBoard = currentGame.board.toMutableList()
+                updateBoard[position] = getPlayer() ?: PlayerType.Empty
+
+                firebaseService.updateGame(
+                    currentGame.copy(board = updateBoard, playerTurn = playerOpposite()!!)
+                )
+            }
         }
     }
 
@@ -40,10 +61,12 @@ class GameViewModel @Inject constructor(
         viewModelScope.launch {
 
             firebaseService.joinToGame(gameId).take(1).collect { game ->
-                val gameResult = game?.copy(player2 = PlayerModel(
-                    userId = userId,
-                    playerType = PlayerType.SecondPlayer
-                ))
+                val gameResult = game?.copy(
+                    player2 = PlayerModel(
+                        userId = userId,
+                        playerType = PlayerType.SecondPlayer
+                    )
+                )
                 gameResult?.let {
                     firebaseService.updateGame(it)
                 }
@@ -56,11 +79,66 @@ class GameViewModel @Inject constructor(
     private fun join(gameId: String) {
         viewModelScope.launch {
             firebaseService.joinToGame(gameId).collect { game ->
-                val gameResult = game?.copy(isGameReady = game.player2 != null, isMyTurn = isMyTurn())
+                val gameResult =
+                    game?.copy(
+                        isGameReady = game.player2 != null,
+                        isMyTurn = isMyTurn(game.playerTurn)
+                    )
                 _game.update { gameResult }
+                verifyWinner()
             }
         }
     }
 
-    private fun isMyTurn(): Boolean = game.value?.playerTurn?.userId == userId
+    private fun isMyTurn(playerTurn: PlayerModel): Boolean = playerTurn.userId == userId
+
+    private fun getPlayer(): PlayerType? {
+        return when {
+            (game.value?.player1?.userId == userId) -> PlayerType.FirstPlayer
+            (game.value?.player2?.userId == userId) -> PlayerType.SecondPlayer
+            else -> null
+        }
+    }
+
+    private fun playerOpposite(): PlayerModel? {
+        return when {
+            (game.value?.player1?.userId == userId) -> _game.value?.player2
+            (game.value?.player2?.userId == userId) -> _game.value?.player1
+            else -> null
+        }
+    }
+
+    private fun verifyWinner() {
+        val board = _game.value?.board
+        if (board != null && board.size == 9) {
+            _winner.update {
+                when {
+                    isGameWon(board, PlayerType.FirstPlayer) -> PlayerType.FirstPlayer
+                    isGameWon(board, PlayerType.SecondPlayer) -> PlayerType.SecondPlayer
+                    else -> null
+                }
+            }
+        }
+    }
+
+    private fun isGameWon(board: List<PlayerType>, playerType: PlayerType): Boolean {
+        return when {
+            // Row
+            (board[0] == playerType && board[1] == playerType && board[2] == playerType) -> true
+            (board[3] == playerType && board[4] == playerType && board[5] == playerType) -> true
+            (board[6] == playerType && board[7] == playerType && board[8] == playerType) -> true
+
+            // Columns
+            (board[0] == playerType && board[3] == playerType && board[6] == playerType) -> true
+            (board[1] == playerType && board[4] == playerType && board[7] == playerType) -> true
+            (board[2] == playerType && board[5] == playerType && board[8] == playerType) -> true
+
+            // Diagonals
+            (board[0] == playerType && board[4] == playerType && board[8] == playerType) -> true
+            (board[2] == playerType && board[4] == playerType && board[6] == playerType) -> true
+
+            else -> false
+        }
+
+    }
 }
