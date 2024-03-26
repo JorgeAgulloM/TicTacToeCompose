07ee45e42e7ecfcfe324e5c2d72f1ab414a132bf
Jorge1 <agullojorge@gmail.com>
2024-03-26 19:43:56 +0100

    Implemented game visual/interface

--- app/src/main/java/com/softyorch/tictactoecompose/ui/game/GameScreen.kt
+++ app/src/main/java/com/softyorch/tictactoecompose/ui/game/GameScreen.kt
@@ -3,6 +3,7 @@ package com.softyorch.tictactoecompose.ui.game
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.softyorch.tictactoecompose.ui.model.GameModel
+import com.softyorch.tictactoecompose.ui.model.PlayerType
 
 
 @Composable
@@ -36,16 +38,25 @@ fun GameScreen(
     }
 
     val game: GameModel? by viewModel.game.collectAsState()
+    val winner: PlayerType? by viewModel.winner.collectAsState()
 
-    Board(game)
+    if (winner != null)
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            val winnerPlayer = if (winner == PlayerType.FirstPlayer) "Player 1" else "Player 2"
+            Text(text = "Ganador!!\n$winnerPlayer")
+        }
+    else
+        Board(game, onItemSelected = { position -> viewModel.onItemSelected(position) })
 }
 
 @Composable
-fun Board(game: GameModel?) {
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = game?.gameId.orEmpty())
+fun Board(game: GameModel?, onItemSelected: (Int) -> Unit) {
+    if (game == null) return
 
-        val status = if (game?.isGameReady == true) {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = game.gameId)
+
+        val status = if (game.isGameReady) {
             if (game.isMyTurn) {
                 "Tu turno"
             } else {
@@ -58,32 +69,32 @@ fun Board(game: GameModel?) {
         Text(text = status)
 
         Row {
-            GameItem()
-            GameItem()
-            GameItem()
+            GameItem(game.board[0]) { onItemSelected(0) }
+            GameItem(game.board[1]) { onItemSelected(1) }
+            GameItem(game.board[2]) { onItemSelected(2) }
         }
         Row {
-            GameItem()
-            GameItem()
-            GameItem()
+            GameItem(game.board[3]) { onItemSelected(3) }
+            GameItem(game.board[4]) { onItemSelected(4) }
+            GameItem(game.board[5]) { onItemSelected(5) }
         }
         Row {
-            GameItem()
-            GameItem()
-            GameItem()
+            GameItem(game.board[6]) { onItemSelected(6) }
+            GameItem(game.board[7]) { onItemSelected(7) }
+            GameItem(game.board[8]) { onItemSelected(8) }
         }
     }
 }
 
 @Composable
-fun GameItem() {
+fun GameItem(playerType: PlayerType, onItemSelected: () -> Unit) {
     Box(
         modifier = Modifier
             .padding(12.dp)
             .size(64.dp)
-            .border(BorderStroke(2.dp, color = Color.Black)),
+            .border(BorderStroke(2.dp, color = Color.Black)).clickable { onItemSelected() },
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "X")
+        Text(text = playerType.symbol)
     }
 }
